628c4e08d1f30acb44cd23f336427acc2b6d229d
Joffrey Peters <joffrey.peters@whoi.edu>
2024-02-07 21:41:00 -0500

    Updating JSON format for parameters and qcconfig columns in qartod_tables to be proper JSON. Also a huge number of formatting changes thanks to black.

--- scripts/load_qartod.py
+++ scripts/load_qartod.py
@@ -3,6 +3,11 @@ import collections
 import argparse
 import getpass
 import json
+import sys
+
+if sys.version.startswith("3"):
+    raw_input = input
+
 import psycopg2
 from psycopg2 import sql
 
@@ -15,6 +20,7 @@ from psycopg2 import sql
 # QARTOD Requirements and Action Items - CGSN CTD Gross Range Lookup.csv:  This is a working sheet of CGSN that was used to collect gross range config data. The prior CSV should contain this data, but in case there is a need to load from this CSV, support for its format has been included in this script.
 # ClimatologyTestInputs.csv:  New format for climatology config data - the climatologyTable column points to individual CSV files for each climatology qcconfig - see RS01SBPS-SF01A-2A-CTDPFA102-practical_salinity.csv for an example.
 
+
 def parse_number(string):
     try:
         return int(string)
@@ -23,87 +29,104 @@ def parse_number(string):
 
 
 def parse_qartod_dict(qartod_dict):
-    subsite = qartod_dict.get('subsite')
-    refdes = qartod_dict.get('Reference Designator')
+    subsite = qartod_dict.get("subsite")
+    refdes = qartod_dict.get("Reference Designator")  # No examples in current qc-lookup
     if subsite:
-        if subsite.startswith('#'):
-            return
+        if subsite.startswith("#"):  # commented out
+            return None
         # Climatology Test Inputs
-        node = qartod_dict.get('node')
-        sensor = qartod_dict.get('sensor')
+        node = qartod_dict.get("node")
+        sensor = qartod_dict.get("sensor")
     elif refdes:
         # CTD Gross Range Lookup
-        subsite, node, sensor = refdes.split('-', 2)
+        subsite, node, sensor = refdes.split("-", 2)
     else:
         # CGSN CTD Gross Range Lookup
-        refdes = qartod_dict.get('refdes')
-        subsite, node, sensor = refdes.split('-', 2)
-        
-    stream = qartod_dict.get('stream')
+        refdes = qartod_dict.get("refdes")  # No examples in current qc-lookup
+        subsite, node, sensor = refdes.split("-", 2)
+
+    stream = qartod_dict.get("stream")
     if not stream:
-        stream = qartod_dict.get('Stream Identifier')
-    
-    parameters = qartod_dict.get('parameters')
+        # No examples of this element in current qc-lookup
+        stream = qartod_dict.get("Stream Identifier")
+
+    parameters = qartod_dict.get("parameters")
     if not parameters:
         # CTD Gross Range Lookup
-        param = qartod_dict.get('Parameter Name')
+        param = qartod_dict.get("Parameter Name")  # No examples in current qc-lookup
         if not param:
             # CGSN CTD Gross Range Lookup
-            param = qartod_dict.get('particleKey')
+            param = qartod_dict.get("particleKey")
         # convert legacy 'parameter' field into new 'parameters' dictionary
-        parameters = "{'inp': '%s'}" % param
+        parameters = '{"inp": "%s"}' % param
+    else:
+        parameters = parameters.replace("'", '"')
 
-    qcconfig = qartod_dict.get('qcConfig')
-    climatologyTable = qartod_dict.get('climatologyTable')
+    qcconfig = qartod_dict.get("qcConfig")
+    climatologyTable = qartod_dict.get("climatologyTable")
     if not qcconfig and climatologyTable:
-         # Extract configuration details for test inputs referring to dataset variables
-        params = parameters
+        # Extract configuration details for test inputs referring to dataset variables
         # single quoted strings in parameters (i.e. from the database field) will mess up the json.loads call
-        params = params.replace("'", "\"")
+        params = parameters.replace("'", '"')
         param_dict = {}
         try:
-            param_dict = json.loads(params)      
+            param_dict = json.loads(params)
         except ValueError:
-            print('Failure deserializing QC parameter configuration ', params)
-            return        
+            print("Failure deserializing QC parameter configuration ", params)
+            return
         # climatology qcconfig is encoded in a separate CSV file pointed to by climatologyTable
         qcconfig = parse_climatology_table(climatologyTable, param_dict)
     elif not qcconfig:
-        qcconfig = {'qartod': {'gross_range_test': {}}}
+        qcconfig = {"qartod": {"gross_range_test": {}}}
         # suspect_span is optional
-        suspect_span_string = qartod_dict.get('User (min, max)')
+        suspect_span_string = qartod_dict.get("User (min, max)")
         if not suspect_span_string:
             # CGSN CTD Gross Range Lookup
-            suspect_span_string = qartod_dict.get('user_range')
+            suspect_span_string = qartod_dict.get("user_range")
         if suspect_span_string:
-            suspect_span = [parse_number(x) for x in suspect_span_string[1:-1].split(', ')]
-            qcconfig['qartod']['gross_range_test']['suspect_span'] = suspect_span
-    
+            suspect_span = [
+                parse_number(x) for x in suspect_span_string[1:-1].split(", ")
+            ]
+            qcconfig["qartod"]["gross_range_test"]["suspect_span"] = suspect_span
+
         # fail_span is not optional
-        fail_span_string = qartod_dict.get('Sensor (min, max)')
+        fail_span_string = qartod_dict.get("Sensor (min, max)")
         if not fail_span_string:
-            fail_span_string = qartod_dict['gross_range']
-        fail_span = [parse_number(x) for x in fail_span_string[1:-1].split(', ')]
-        qcconfig['qartod']['gross_range_test']['fail_span'] = fail_span
+            fail_span_string = qartod_dict["gross_range"]
+        fail_span = [parse_number(x) for x in fail_span_string[1:-1].split(", ")]
+        qcconfig["qartod"]["gross_range_test"]["fail_span"] = fail_span
         qcconfig = str(qcconfig)
 
     # do some data sanitation - valid JSON uses null instead None
-    qcconfig = qcconfig.replace('None', 'null')
-    parameters = parameters.replace('None', 'null')
-    source = qartod_dict.get('source')
+    qcconfig = qcconfig.replace("'", '"')
+    qcconfig = qcconfig.replace("None", "null")
+    parameters = parameters.replace("None", "null")
+    source = qartod_dict.get("source")
     if not source:
-        source = qartod_dict.get('Source')
+        source = qartod_dict.get("Source")
 
-    return collections.OrderedDict({'id': None, 'subsite': subsite, 'node': node, 'sensor': sensor, 'stream': stream, 
-                                    'parameters': parameters, 'qcconfig': qcconfig, 'source': source})
+    return collections.OrderedDict(
+        {
+            "id": None,
+            "subsite": subsite,
+            "node": node,
+            "sensor": sensor,
+            "stream": stream,
+            "parameters": parameters,
+            "qcconfig": qcconfig,
+            "source": source,
+        }
+    )
 
 
 def parse_qartod_file(filepath):
     qartod_list = []
-    with open(filepath, mode='r') as csvfile:
+    with open(filepath, mode="r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            qartod_list.append(parse_qartod_dict(row))
+            qartod_entry = parse_qartod_dict(row)
+            if qartod_entry is not None:
+                qartod_list.append(qartod_entry)
     return qartod_list
 
 
@@ -112,15 +135,17 @@ def parse_climatology_table(filepath, param_dict):
     suffix = "]}}}"
 
     configs = []
-    with open(filepath, mode='r') as csvfile:
+    with open(filepath, mode="r") as csvfile:
         reader = csv.reader(csvfile)
         # NOTE: time_bins[0] is empty for alignment purposes!
-        time_bins = reader.next()
+        time_bins = next(reader)
 
-        #don't add a zspan if there is no value for zinp, doing so will cause tests to  not run
+        # don't add a zspan if there is no value for zinp, doing so will cause tests to  not run
         includeZSpan = True
-        if param_dict and (param_dict['zinp'] == 'null' or param_dict['zinp'] == 'None'):
-               includeZSpan = False
+        if param_dict and (
+            param_dict["zinp"] == "null" or param_dict["zinp"] == "None"
+        ):
+            includeZSpan = False
 
         for row in reader:
             if row:
@@ -128,60 +153,91 @@ def parse_climatology_table(filepath, param_dict):
                 # skip first row which is blank for time_bins and the pressure_bin data for other rows
                 for i in range(1, len(time_bins)):
                     if includeZSpan:
-                        data = "{'tspan': %s, 'zspan': %s, 'vspan': %s, 'period': 'month'}" % (time_bins[i], pressure_bin, row[i])
+                        data = (
+                            "{'tspan': %s, 'zspan': %s, 'vspan': %s, 'period': 'month'}"
+                            % (time_bins[i], pressure_bin, row[i])
+                        )
                     else:
-                        data = "{'tspan': %s, 'vspan': %s, 'period': 'month'}" % (time_bins[i], row[i])
+                        data = "{'tspan': %s, 'vspan': %s, 'period': 'month'}" % (
+                            time_bins[i],
+                            row[i],
+                        )
                     configs.append(data)
-    return '%s%s%s' % (prefix, ', '.join(configs), suffix)
+    return "%s%s%s" % (prefix, ", ".join(configs), suffix)
 
 
-def insert_qartod_records(qartod_list, hostname = None, username = None, password = None):
-    host = hostname or raw_input('Connecting to PostgreSQL to insert QARTOD records...\nhost: ')
-    username = username or raw_input('Connecting to PostgreSQL to insert QARTOD records...\nusername: ')
-    password = password or getpass.getpass('password: ')
+def insert_qartod_records(qartod_list, hostname=None, username=None, password=None):
+    host = hostname or raw_input(
+        "Connecting to PostgreSQL to insert QARTOD records...\nhost: "
+    )
+    username = username or raw_input(
+        "Connecting to PostgreSQL to insert QARTOD records...\nusername: "
+    )
+    password = password or getpass.getpass("password: ")
     connection = None
     try:
-        connection = psycopg2.connect(user=username, password=password, host=host, port='5432', database='metadata')
+        connection = psycopg2.connect(
+            user=username,
+            password=password,
+            host=host,
+            port="5432",
+            database="metadata",
+        )
         for parsed_qartod_dict in qartod_list:
             if parsed_qartod_dict:
-
                 with connection:
                     with connection.cursor() as cursor:
                         cursor.execute("SELECT nextval('qartod_test_sequence')")
                         qartod_id = cursor.fetchone()
-                        parsed_qartod_dict['id'] = qartod_id
-                
-                        fields = sql.SQL(', ').join(map(sql.Identifier, parsed_qartod_dict.keys()))
-                        values = sql.SQL(', ').join(map(sql.Placeholder, parsed_qartod_dict.keys()))
-                        cursor.execute(sql.SQL("INSERT INTO qartod_tests ({}) VALUES ({})").format(fields, values), parsed_qartod_dict)
+                        parsed_qartod_dict["id"] = qartod_id
+
+                        fields = sql.SQL(", ").join(
+                            map(sql.Identifier, parsed_qartod_dict.keys())
+                        )
+                        values = sql.SQL(", ").join(
+                            map(sql.Placeholder, parsed_qartod_dict.keys())
+                        )
+                        cursor.execute(
+                            sql.SQL("INSERT INTO qartod_tests ({}) VALUES ({})").format(
+                                fields, values
+                            ),
+                            parsed_qartod_dict,
+                        )
     finally:
         if connection:
             connection.close()
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Parse a CSV indicating QARTOD test records.")
-    parser.add_argument("file", type=str, help="filepath for the QARTOD test CSV file to parse")
-    args = parser.parse_args()
-    data = parse_qartod_file(args.file)
+def main(filepath=None):
+    if filepath is None:
+        parser = argparse.ArgumentParser(
+            description="Parse a CSV indicating QARTOD test records."
+        )
+        parser.add_argument(
+            "file", type=str, help="filepath for the QARTOD test CSV file to parse"
+        )
+        args = parser.parse_args()
+        filepath = args.file
+    data = parse_qartod_file(filepath)
     # Try to pick up database credentials from environment
     import os
 
     try:
-        hostname = os.environ['POSTGRES_HOSTNAME']
+        hostname = os.environ["POSTGRES_HOSTNAME"]
     except KeyError:
         hostname = None
     try:
-        username = os.environ['POSTGRES_USERNAME']
+        username = os.environ["POSTGRES_USERNAME"]
     except KeyError:
         username = None
     try:
-        password = os.environ['POSTGRES_PASSWORD']
+        password = os.environ["POSTGRES_PASSWORD"]
     except KeyError:
         password = None
-    
+
     insert_qartod_records(data, hostname=hostname, username=username, password=password)
 
 
+if __name__ == "__main__":
 if __name__ == '__main__':
     main()
